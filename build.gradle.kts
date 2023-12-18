c12069c686fd9c3dc7363ff24e57d30bd744672e
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-12-19 02:47:52 +0300

    первый коммит

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "org.openjsr"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
