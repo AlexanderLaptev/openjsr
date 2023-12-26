e5c1dd16661f5d3b3c21c50d8490312bdda21242
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-12-27 01:43:12 +0300

    переход на унифицированный релиз библиотеки математики 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ application {
 }
 
 dependencies {
-    implementation(":MathLib-2.0.0-module")
+    implementation(":MathLib-2.0.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
