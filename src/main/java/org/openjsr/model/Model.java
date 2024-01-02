c6881648005c0f8949f4df9a3e97230054a3c15b
imbeer <76579340+imbeer@users.noreply.github.com>
2024-01-02 13:25:55 +0300

    изменено представление модели и полигона с хранения объектов вершины на хранение координат и их индексов

--- src/main/java/org/openjsr/model/Model.java
+++ src/main/java/org/openjsr/model/Model.java
@@ -1,113 +1,54 @@
 package org.openjsr.model;
 
+import cg.vsu.render.math.vector.Vector2f;
 import cg.vsu.render.math.vector.Vector3f;
 import org.openjsr.core.Transform;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Данный класс хранит в себе вершины, полигоны и <b>Transform</b> модели.
- */
 public class Model {
-    public Model() {}
 
-    /**
-     * Создает модель с заданным списком полигонов и вершин. При это модель принимает нулевое положение в мире.
-     * @param polygons список полигонов
-     * @param vertices список вершин
-     */
-    public Model(List<Polygon> polygons, List<Vertex> vertices) {
-        this.polygons = polygons;
-        this.vertices = vertices;
+    public Model() {
     }
 
-    /**
-     * Создает модель с заданным списком полигонов, вершин и трансформацией (масштабом, поворотом и сдвигом)
-     * @param polygons список полигонов
-     * @param vertices список вершин
-     * @param transform заданное положение
-     */
-    public Model(List<Polygon> polygons, List<Vertex> vertices, Transform transform) {
-        this.polygons = polygons;
+    public Model(
+            List<Vector3f> vertices,
+            List<Vector3f> normals,
+            List<Vector2f> textureVertices,
+            List<Polygon> polygons,
+            Transform transform) {
+
         this.vertices = vertices;
+        this.normals = normals;
+        this.textureVertices = textureVertices;
+        this.polygons = polygons;
         this.transform = transform;
     }
 
-    /**
-     * Список полигонов (плоскостей).
-     */
-    private List<Polygon> polygons = new ArrayList<>();
+    public Model(
+            List<Vector3f> vertices,
+            List<Vector3f> normals,
+            List<Vector2f> textureVertices,
+            List<Polygon> polygons) {
 
-    /**
-     * Список вершин модели
-     */
-    private List<Vertex> vertices = new ArrayList<>();
-
-    /**
-     * Информация о масштабе, повороте и положении модели в мире.
-     */
-    private Transform transform = new Transform(
-            new Vector3f(new float[]{0f, 0f, 0f}),
-            new Vector3f(new float[]{0f, 0f, 0f}),
-            new Vector3f(new float[]{0f, 0f, 0f}));
-
-
-    public List<Polygon> getPolygons() {
-        return polygons;
-    }
-
-    public void setPolygons(List<Polygon> polygons) {
+        this.vertices = vertices;
+        this.normals = normals;
+        this.textureVertices = textureVertices;
         this.polygons = polygons;
     }
 
-    public List<Vertex> getVertices() {
-        return vertices;
-    }
+    public List<Vector3f> vertices = new ArrayList<>();
 
-    /**
-     * Возвращает оригинальный экземпляр веришны с такими же параметрами, как у копии
-     * @param vertex копия оригинальной вершины с такими же параметрами
-     * @return оригинальную вершина, используемая в модели
-     */
-    public Vertex getOriginalVertex(Vertex vertex) {
-        for (Vertex origin : vertices) {
-            if (origin.equals(vertex)) {
-                return origin;
-            }
-        }
-        return vertex;
-    }
+    public List<Vector3f> normals = new ArrayList<>();
 
-    public void setVertices(List<Vertex> vertices) {
-        this.vertices = vertices;
-    }
+    public List<Vector2f> textureVertices = new ArrayList<>();
 
-    public Transform getTransform() {
-        return transform;
-    }
+    public List<Polygon> polygons = new ArrayList<>();
 
-    public void setTransform(Transform transform) {
-        this.transform = transform;
-    }
-
-    /**
-     * Добавляет вершину в список всех вершин модели
-     * @param vertex добавляемая вершина
-     */
-    public void addVertex(Vertex vertex) {
-        if (!vertices.contains(vertex)) {
-            vertices.add(vertex);
-        }
-    }
-
-    /**
-     * Добавляет полигон в список всех полигонов модели. Добавляет точки полигона, которые не содержатся в общем множестве.
-     * @param polygon добавляемый полигон
-     */
-    public void addPolygon(Polygon polygon) {
-        polygons.add(polygon);
-        for (Vertex vertex : polygon.getVertices()) {
-            addVertex(vertex);
-        }
-    }
+    public Transform transform = new Transform(
+            new Vector3f(new float[]{0.0F, 0.0F, 0.0F}),
+            new Vector3f(new float[]{0.0F, 0.0F, 0.0F}),
+            new Vector3f(new float[]{1.0F, 1.0F, 1.0F})
+    );
 }
--- src/main/java/org/openjsr/model/Polygon.java
+++ src/main/java/org/openjsr/model/Polygon.java
@@ -7,22 +7,38 @@ import java.util.List;
  * Хранилище вершин одного полигона. Не обязательно должен быть треугольником, поэтому перед растеризацией нужно проводить триангуляцию.
  */
 public class Polygon {
+
+    private List<Integer> vertexIndices = new ArrayList<>();
+    private List<Integer> textureVertexIndices = new ArrayList<>();
+    private List<Integer> normalIndices = new ArrayList<>();
+
+
     public Polygon() {}
 
-    public Polygon(List<Vertex> vertices) {
-        this.vertices = vertices;
+    public void setVertexIndices(List<Integer> vertexIndices) {
+        assert vertexIndices.size() >= 3;
+        this.vertexIndices = vertexIndices;
     }
 
-    /**
-     * Список вершин полигона
-     */
-    private List<Vertex> vertices = new ArrayList<>();
-
-    public List<Vertex> getVertices() {
-        return vertices;
+    public void setTextureVertexIndices(List<Integer> textureVertexIndices) {
+        assert textureVertexIndices.size() >= 3;
+        this.textureVertexIndices = textureVertexIndices;
     }
 
-    public void setVertices(List<Vertex> vertices) {
-        this.vertices = vertices;
+    public void setNormalIndices(List<Integer> normalIndices) {
+        assert normalIndices.size() >= 3;
+        this.normalIndices = normalIndices;
+    }
+
+    public List<Integer> getVertexIndices() {
+        return vertexIndices;
+    }
+
+    public List<Integer> getTextureVertexIndices() {
+        return textureVertexIndices;
+    }
+
+    public List<Integer> getNormalIndices() {
+        return normalIndices;
     }
 }
