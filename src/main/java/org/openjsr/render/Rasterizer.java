aaa8221a417b81a412d86a56c2b7203686d3292e
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-01-04 00:25:23 +0300

    реализована сцена с заготовкой растеризатора

---
+++ src/main/java/org/openjsr/render/Rasterizer.java
@@ -0,0 +1,18 @@
+package org.openjsr.render;
+
+import org.openjsr.core.PerspectiveCamera;
+import org.openjsr.render.lighting.LightingModel;
+
+public class Rasterizer {
+    private static final Rasterizer INSTANCE = new Rasterizer();
+
+    private Rasterizer() {}
+
+    public static Rasterizer getInstance() {
+        return INSTANCE;
+    }
+
+    public void drawModel(Model model, PerspectiveCamera camera, LightingModel lightingModel) {
+        // TODO!
+    }
+}
---
+++ src/main/java/org/openjsr/render/Scene.java
@@ -0,0 +1,38 @@
+package org.openjsr.render;
+
+import org.openjsr.core.PerspectiveCamera;
+import org.openjsr.render.lighting.LightingModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Сцена, располагающая модели в трёхмерном пространстве.
+ */
+public class Scene {
+    /**
+     * Список моделей, находящихся на данной сцене.
+     */
+    private final List<Model> models = new ArrayList<>();
+
+    /**
+     * Получает список моделей, находящихся на данной сцене.
+     *
+     * @return список моделей, находящихся на данной сцене.
+     */
+    public List<Model> getModels() {
+        return models;
+    }
+
+    /**
+     * Вызывает отрисовку сцены глобальным растеризатором.
+     *
+     * @param camera        Активная камера.
+     * @param lightingModel Модель освещения.
+     */
+    public void render(PerspectiveCamera camera, LightingModel lightingModel) {
+        for (Model model : models) {
+            Rasterizer.getInstance().drawModel(model, camera, lightingModel);
+        }
+    }
+}
