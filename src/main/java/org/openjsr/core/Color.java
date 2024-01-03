65823343ddb93be1ce5ac7c6b66ca204497279f9
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-01-04 00:50:33 +0300

    добавлен метод Color.isValid()

--- src/main/java/org/openjsr/core/Color.java
+++ src/main/java/org/openjsr/core/Color.java
@@ -4,27 +4,44 @@ package org.openjsr.core;
  * Изменяемый класс цвета.
  */
 public class Color {
+    /**
+     * Красная компонента цвета.
+     */
     public float red = 0.0f;
 
+    /**
+     * Зелёная компонента цвета.
+     */
     public float green = 0.0f;
 
+    /**
+     * Синяя компонента цвета.
+     */
     public float blue = 0.0f;
 
+    /**
+     * Альфа-канал цвета.
+     */
     public float alpha = 1.0f;
 
-    public Color() {}
+    /**
+     * Создаёт непрозрачный чёрный цвет.
+     */
+    public Color() { }
 
     public Color(float red, float green, float blue, float alpha) {
         this.red = red;
         this.green = green;
         this.blue = blue;
         this.alpha = alpha;
+        assert isValid();
     }
 
     public Color(float red, float green, float blue) {
         this.red = red;
         this.green = green;
         this.blue = blue;
+        assert isValid();
     }
 
     public Color(int red, int green, int blue, int alpha) {
@@ -32,12 +49,14 @@ public class Color {
         this.green = green / 255.0f;
         this.blue = blue / 255.0f;
         this.alpha = alpha / 255.0f;
+        assert isValid();
     }
 
     public Color(int red, int green, int blue) {
         this.red = red / 255.0f;
         this.green = green / 255.0f;
         this.blue = blue / 255.0f;
+        assert isValid();
     }
 
     public int getIntRed() {
@@ -56,7 +75,22 @@ public class Color {
         return (int) (255.0f * alpha);
     }
 
+    /**
+     * Возвращает true, если ни одна компонента цвета не превышает единицы.
+     *
+     * @return true, если ни одна компонента цвета не превышает единицы.
+     */
+    public boolean isValid() {
+        return red <= 1.0f && green <= 1.0f && blue <= 1.0f && alpha <= 1.0f;
+    }
+
+    /**
+     * Конвертирует цвет в 32-битное целое в формате ARGB.
+     *
+     * @return 32-битное целое в формате ARGB, соответствующее данному цвету.
+     */
     public int toArgb() {
+        assert isValid();
         int a = getIntAlpha();
         int r = getIntRed();
         int g = getIntGreen();
