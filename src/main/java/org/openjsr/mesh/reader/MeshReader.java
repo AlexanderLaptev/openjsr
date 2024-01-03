1339b9d8961a2f01a4bc7baeaf2494c6f761e39a
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-01-04 01:38:55 +0300

    добавлена недостающая документация в интерфейсе MeshReader

--- src/main/java/org/openjsr/mesh/reader/MeshReader.java
+++ src/main/java/org/openjsr/mesh/reader/MeshReader.java
@@ -5,6 +5,16 @@ import org.openjsr.mesh.Mesh;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * MeshReader читает полигональную сетку из файла.
+ */
 public interface MeshReader {
+    /**
+     * Читает полигональную сетку из данного файла.
+     *
+     * @param file Файл с описанием сетки.
+     * @return Сетка, прочитанная из файла.
+     * @throws IOException При любой ошибке ввода/вывода.
+     */
     Mesh read(File file) throws IOException;
 }
