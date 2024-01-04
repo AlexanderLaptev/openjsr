e1676ba7296bddb0107da03ba457d30dca6424ae
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-01-04 23:29:21 +0300

    добавлена заготовка метода onCanvasResized() в контроллере главного окна

--- src/main/java/org/openjsr/app/MainWindowController.java
+++ src/main/java/org/openjsr/app/MainWindowController.java
@@ -1,9 +1,13 @@
 package org.openjsr.app;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
+import org.openjsr.render.framebuffer.CanvasFramebuffer;
+import org.openjsr.render.framebuffer.Framebuffer;
 
 public class MainWindowController {
+    private Framebuffer framebuffer;
 
     @FXML
     public void initialize() {
@@ -17,4 +21,11 @@ public class MainWindowController {
         alert.showAndWait();
     }
 
+    /**
+     * Пересоздаёт framebuffer при изменении размера canvas.
+     * @param canvas Canvas, который изменил размер.
+     */
+    private void onCanvasResized(Canvas canvas) {
+        framebuffer = new CanvasFramebuffer(canvas);
+    }
 }
