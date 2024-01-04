524cfea1bdd951b81a87ffa8185953db9185f515
imbeer <76579340+imbeer@users.noreply.github.com>
2024-01-04 21:43:52 +0300

    добавлена интерполяция по барицентрическим координатам

--- src/main/java/org/openjsr/util/GeometryUtils.java
+++ src/main/java/org/openjsr/util/GeometryUtils.java
@@ -8,10 +8,17 @@ import cg.vsu.render.math.vector.Vector2f;
 public class GeometryUtils {
 
     /**
-     * Решает систему уравнений барицентрических координат методом Крамера и возвращает массив барицентрических координат (alpha, beta, gamma)
-     * Углы треугольника будут принимать значения (1, 0, 0), (0, 1, 0), (0, 0, 1) в зависимости от порядка их задания.
-     * @param dotX x точки, координаты которой нужно найти
-     * @param dotY y точки, координаты которой нужно найти
+     * @param point    двумерный вектор координат точки, барицентрические координаты которой нужно найти
+     * @param triangle массив двумерных векторов координат вершин треугольника
+     * @return массив барицентрических координат (alpha, beta, gamma)
+     */
+    public static float[] getBarycentricCoords(Vector2f point, Vector2f[] triangle) {
+        return getBarycentricCoords(point.x, point.y, triangle);
+    }
+
+    /**
+     * @param dotX     x точки, координаты которой нужно найти
+     * @param dotY     y точки, координаты которой нужно найти
      * @param triangle массив двумерных векторов координат вершин треугольника (задан по часовой стрелке)
      * @return float[] массив барицентрических координат (alpha, beta, gamma)
      */
@@ -23,6 +30,34 @@ public class GeometryUtils {
         float bY = triangle[1].y;
         float cY = triangle[2].y;
 
+        return getBarycentricCoords(
+                dotX, dotY,
+                aX, aY,
+                bX, bY,
+                cX, cY
+        );
+    }
+
+    /**
+     * Решает систему уравнений барицентрических координат методом Крамера и возвращает массив барицентрических координат (alpha, beta, gamma)
+     * Углы треугольника будут принимать значения (1, 0, 0), (0, 1, 0), (0, 0, 1) в зависимости от порядка их задания.
+     *
+     * @param dotX x точки, координаты который нужно найти
+     * @param dotY y точки, координаты которой нужно найти
+     * @param aX   x веришны A
+     * @param aY   y веришны A
+     * @param bX   x веришны B
+     * @param bY   y веришны B
+     * @param cX   x веришны C
+     * @param cY   y веришны C
+     * @return float[] массив барицентрических координат (alpha, beta, gamme)
+     */
+    public static float[] getBarycentricCoords(
+            float dotX, float dotY,
+            float aX, float aY,
+            float bX, float bY,
+            float cX, float cY
+    ) {
         float denominator = (bY - cY) * (aX - cX) + (cX - bX) * (aY - cY);
         float alpha = ((bY - cY) * (dotX - cX) + (cX - bX) * (dotY - cY)) / denominator;
         float beta = ((cY - aY) * (dotX - cX) + (aX - cX) * (dotY - cY)) / denominator;
@@ -32,11 +67,13 @@ public class GeometryUtils {
     }
 
     /**
-     * @param point двумерный вектор координат точки, барицентрические координаты которой нужно найти
-     * @param triangle массив двумерных векторов координат вершин треугольника
-     * @return массив барицентрических координат (alpha, beta, gamma)
+     * Интерполирует значения по барицентрическим координатам
+     *
+     * @param cords  барицентрические координаты
+     * @param values значения в вершинах
+     * @return единственное интерполированное значение
      */
-    public static float[] getBarycentricCoords(Vector2f point, Vector2f[] triangle) {
-        return getBarycentricCoords(point.x, point.y, triangle);
+    public static float interpolate(float[] cords, float[] values) {
+        return cords[0] * values[0] + cords[1] * values[1] + cords[2] * values[2];
     }
 }
