77ca2d42a25cc79be050ee24b9459958ce57c570
imbeer <76579340+imbeer@users.noreply.github.com>
2024-01-05 13:22:57 +0300

    добавлено открытие файла, создание сцены и фиксированный размер canvas (временно)

--- src/main/java/org/openjsr/app/MainWindowController.java
+++ src/main/java/org/openjsr/app/MainWindowController.java
@@ -1,16 +1,49 @@
 package org.openjsr.app;
 
+import cg.vsu.render.math.vector.Vector3f;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.openjsr.mesh.Face;
+import org.openjsr.mesh.Mesh;
+import org.openjsr.mesh.reader.MeshReader;
+import org.openjsr.mesh.reader.ObjReader;
+import org.openjsr.mesh.reader.ObjReaderException;
+import org.openjsr.mesh.triangulation.SimpleTriangulator;
+import org.openjsr.mesh.triangulation.TriangulatedMesh;
+import org.openjsr.mesh.triangulation.Triangulator;
+import org.openjsr.render.Model;
+import org.openjsr.render.Scene;
 import org.openjsr.render.framebuffer.CanvasFramebuffer;
 import org.openjsr.render.framebuffer.Framebuffer;
+import org.openjsr.render.lighting.FlatLightingModel;
+import org.openjsr.render.lighting.FullbrightLightingModel;
+import org.openjsr.render.shader.UniformColorShader;
+import org.openjsr.core.PerspectiveCamera;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class MainWindowController {
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private VBox root;
     private Framebuffer framebuffer;
+    private Scene scene;
+    private PerspectiveCamera camera;
 
     @FXML
     public void initialize() {
+        canvas.setWidth(1600);
+        canvas.setHeight(900);
+        onCanvasResized();
     }
 
     private void showAlert(String message) {
@@ -25,7 +58,47 @@ public class MainWindowController {
      * Пересоздаёт framebuffer при изменении размера canvas.
      * @param canvas Canvas, который изменил размер.
      */
-    private void onCanvasResized(Canvas canvas) {
+    private void onCanvasResized() {
         framebuffer = new CanvasFramebuffer(canvas);
+    }
+
+    @FXML
+    private void onOpenFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выберите файл");
+        Stage stage = (Stage) root.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            MeshReader reader = new ObjReader();
+            Mesh mesh = new Mesh();
+            try {
+                mesh = reader.read(selectedFile);
+            } catch (IOException | ObjReaderException e) {
+                showAlert(e.getMessage());
+            }
+            if (scene == null) {
+                onCreateNewScene();
+            }
+                Triangulator triangulator = new SimpleTriangulator();
+                List<Face> triangles = triangulator.triangulateFaces(mesh.faces);
+                TriangulatedMesh triangulatedMesh = new TriangulatedMesh(mesh, triangles);
+                Model model = new Model(triangulatedMesh, new UniformColorShader());
+                scene.getModels().add(model);
+                scene.render(camera, new FullbrightLightingModel(), framebuffer);
+        }
+    }
+
+    @FXML
+    private void onCreateNewScene() {
+        scene = new Scene();
+        camera = new PerspectiveCamera();
+        camera.setPosition(new Vector3f(5, 5, 5));
+        camera.setViewTarget(new Vector3f(0, 0, 0));
+        framebuffer = new CanvasFramebuffer(canvas);
+    }
+
+    @FXML
+    private void onMouseClicked(MouseEvent mouseEvent) {
+        System.out.println("clicked");
     }
 }
