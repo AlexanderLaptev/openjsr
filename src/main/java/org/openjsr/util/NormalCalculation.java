5b717c706901e2ec796aefbe3d9e6e0bc4d83a04
heathlisss <heathlisss@gmail.com>
2024-01-05 20:04:24 +0300

    расчет нормалей

---
+++ src/main/java/org/openjsr/util/NormalCalculation.java
@@ -0,0 +1,63 @@
+package org.openjsr.util;
+
+import cg.vsu.render.math.vector.Vector3f;
+import org.openjsr.mesh.Face;
+import org.openjsr.mesh.Mesh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NormalCalculation {
+
+    /**
+     * Проходит по полигонам модели, вычисляя нормали полигонов и сумму нормалей каждой вершины.
+     * нормализует каждую нормаль вершиын.
+     *
+     * @param mesh Модель.
+     * @return Список трёхмерных векторов (нормалей вершин).
+     */
+    public static List<Vector3f> normalsVertex(Mesh mesh) {
+        List<Vector3f> normalsVertex = new ArrayList<Vector3f>();
+        List<Vector3f> normalsPolygon = new ArrayList<Vector3f>();
+
+        Integer[] count = new Integer[mesh.vertices.size()];
+        Vector3f[] normalSummaVertex = new Vector3f[mesh.vertices.size()];
+
+        for (int indexPoligon = 0; indexPoligon < mesh.faces.size(); indexPoligon++) {
+            normalsPolygon.add(indexPoligon, normalPolygon(mesh.faces.get(indexPoligon), mesh.vertices));
+
+            List<Integer> vertexIndices = mesh.faces.get(indexPoligon).getVertexIndices();
+
+            mesh.faces.get(indexPoligon).setNormalIndices(vertexIndices);
+
+            for (Integer vertexIndex : vertexIndices) {
+                if (normalSummaVertex[vertexIndex] == null) {
+                    normalSummaVertex[vertexIndex] = new Vector3f(normalsPolygon.get(indexPoligon));
+                    count[vertexIndex] = 1;
+                } else {
+                    normalSummaVertex[vertexIndex].add(normalsPolygon.get(indexPoligon));
+                    count[vertexIndex]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            normalsVertex.add(i, normalSummaVertex[i].div(count[i]).div(normalSummaVertex[i].div(count[i]).len()));
+        }
+        return normalsVertex;
+    }
+
+    private static Vector3f normalPolygon(Face face, List<Vector3f> vertices) {
+        List<Integer> vertexIndices = face.getVertexIndices();
+        try {
+            return vector(vertices.get(vertexIndices.get(0)),vertices.get(vertexIndices.get(1))).crs(vector(vertices.get(vertexIndices.get(0)),vertices.get(vertexIndices.get(2))));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("известно менее 3 вершин (у полигона)");
+        }
+        return null;
+    }
+
+    private static Vector3f vector(Vector3f v1, Vector3f v2) {
+        return new Vector3f(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+    }
+}
---
+++ src/test/java/org/openjsr/util/NormalCalculationTest.java
@@ -0,0 +1,52 @@
+package org.openjsr.util;
+
+import cg.vsu.render.math.vector.Vector3f;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openjsr.mesh.Face;
+import org.openjsr.mesh.Mesh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NormalCalculationTest {
+    @Test
+    void normalsVertex() {
+        Mesh mesh = new Mesh();
+        mesh.vertices = new ArrayList<>(List.of(new Vector3f[]{
+                new Vector3f(1.0f, 0.0f, 1.0f),
+                new Vector3f(1.0f, 0.0f, 0.0f),
+                new Vector3f(0.0f, 1.0f, 0.0f),
+                new Vector3f(0.0f, 1.0f, 1.0f),
+                new Vector3f(0.0f, 0.0f, 1.0f),
+                new Vector3f(0.0f, 0.0f, 0.0f)
+        }));
+        mesh.faces = new ArrayList<>(List.of(new Face[]{
+                new Face(),
+                new Face(),
+                new Face(),
+                new Face(),
+                new Face()
+        }));
+        mesh.faces.get(0).setVertexIndices(List.of(new Integer[]{0, 1, 2, 3}));
+        mesh.faces.get(1).setVertexIndices(List.of(new Integer[]{3, 2, 5, 4}));
+        mesh.faces.get(2).setVertexIndices(List.of(new Integer[]{0, 4, 5, 1}));
+        mesh.faces.get(3).setVertexIndices(List.of(new Integer[]{1, 5, 2}));
+        mesh.faces.get(4).setVertexIndices(List.of(new Integer[]{0, 3, 4}));
+
+        List<Vector3f> resultNormalsVertex = NormalCalculation.normalsVertex(mesh);
+        List<Vector3f> expectedResultNormalsVertex = new ArrayList<>(List.of(new Vector3f[]{
+                new Vector3f((float) (1.0f / Math.sqrt(2)), 0.0f, (float) (1.0f / Math.sqrt(2))),
+                new Vector3f((float) (1.0f / Math.sqrt(2)), 0.0f, (float) (-1.0f / Math.sqrt(2))),
+                new Vector3f(0.0f, (float) (1.0f / Math.sqrt(2)), (float) (-1.0f / Math.sqrt(2))),
+                new Vector3f(0.0f, (float) (1.0f / Math.sqrt(2)), (float) (1.0f / Math.sqrt(2))),
+                new Vector3f((float) (-1.0f / Math.sqrt(3)), (float) (-1.0f / Math.sqrt(3)), (float) (1.0f / Math.sqrt(3))),
+                new Vector3f((float) (-1.0f / Math.sqrt(3)), (float) (-1.0f / Math.sqrt(3)), (float) (-1.0f / Math.sqrt(3)))
+        }));
+        for (int i = 0; i < resultNormalsVertex.size(); i++) {
+            assertTrue(resultNormalsVertex.get(i).equals(expectedResultNormalsVertex.get(i)));
+        }
+    }
+}
