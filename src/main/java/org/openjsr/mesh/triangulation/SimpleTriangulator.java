a1d71b345b37039b53180f6544619731d75ae2cc
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-01-05 21:07:52 +0300

    исправлена ошибка с выходом за пределы списка при триангуляции сетки в SimpleTriangulator

--- src/main/java/org/openjsr/mesh/triangulation/SimpleTriangulator.java
+++ src/main/java/org/openjsr/mesh/triangulation/SimpleTriangulator.java
@@ -26,24 +26,34 @@ public class SimpleTriangulator implements Triangulator {
 
         for (int verListInd = 2; verListInd < face.getVertexIndices().size(); verListInd++) {
             Face triangle = new Face();
+            // Списки вершин/текстурных вершин/нормалей в треугольниках имеют фиксированный размер.
             triangle.setVertexIndices(
                     Arrays.asList(
                             face.getVertexIndices().get(0),
                             face.getVertexIndices().get(verListInd - 1),
-                            face.getVertexIndices().get(verListInd)));
+                            face.getVertexIndices().get(verListInd)
+                    ));
 
-            triangle.setNormalIndices(
-                    Arrays.asList(
-                            face.getNormalIndices().get(0),
-                            face.getNormalIndices().get(verListInd - 1),
-                            face.getNormalIndices().get(verListInd)));
+            if (!face.getNormalIndices().isEmpty()) {
+                triangle.setNormalIndices(
+                        Arrays.asList(
+                                face.getNormalIndices().get(0),
+                                face.getNormalIndices().get(verListInd - 1),
+                                face.getNormalIndices().get(verListInd)
+                        )
+                );
+            }
 
-            triangle.setTextureVertexIndices(
-                    Arrays.asList(
-                            face.getTextureVertexIndices().get(0),
-                            face.getTextureVertexIndices().get(verListInd - 1),
-                            face.getTextureVertexIndices().get(verListInd)));
+            if (!face.getTextureVertexIndices().isEmpty()) {
+                triangle.setTextureVertexIndices(
+                        Arrays.asList(
+                                face.getTextureVertexIndices().get(0),
+                                face.getTextureVertexIndices().get(verListInd - 1),
+                                face.getTextureVertexIndices().get(verListInd)
+                        )
+                );
 
+            }
             triangles.add(triangle);
         }
         return triangles;
