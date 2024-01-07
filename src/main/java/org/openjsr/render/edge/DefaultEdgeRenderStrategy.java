14db78a1d63f8f613f43cd74e86d509ed4fee884
imbeer <76579340+imbeer@users.noreply.github.com>
2024-01-07 12:59:45 +0300

    добавлена отрисовка с проверкой глубины

--- src/main/java/org/openjsr/render/edge/DefaultEdgeRenderStrategy.java
+++ src/main/java/org/openjsr/render/edge/DefaultEdgeRenderStrategy.java
@@ -3,11 +3,12 @@ package org.openjsr.render.edge;
 import cg.vsu.render.math.vector.Vector2f;
 import cg.vsu.render.math.vector.Vector4f;
 import javafx.scene.paint.Color;
+import org.openjsr.render.Rasterizer;
 import org.openjsr.render.framebuffer.CanvasFramebuffer;
 import org.openjsr.render.framebuffer.Framebuffer;
 
 public class DefaultEdgeRenderStrategy implements EdgeRenderStrategy {
-    private boolean isDepthTestEnabled = false;
+    private boolean isDepthTestEnabled = true;
 
     private static final Color DEFAULT_COLOR = Color.RED;
 
@@ -26,18 +27,27 @@ public class DefaultEdgeRenderStrategy implements EdgeRenderStrategy {
         int y2 = (int) vertices[1].y;
         int y3 = (int) vertices[2].y;
 
-        drawEdge(x1, y1, x2, y2, framebuffer);
-        drawEdge(x2, y2, x3, y3, framebuffer);
-        drawEdge(x1, y1, x3, y3, framebuffer);
+        float z1 = vertices[0].z;
+        float z2 = vertices[1].z;
+        float z3 = vertices[2].z;
+
+        drawEdge(x1, y1, z1, x2, y2, z2, framebuffer);
+        drawEdge(x2, y2, z2, x3, y3, z3, framebuffer);
+        drawEdge(x1, y1, z1, x3, y3, z3, framebuffer);
     }
 
     private void drawEdge(
-            int x1, int y1,
-            int x2, int y2,
+            int x1, int y1, float z1,
+            int x2, int y2, float z2,
             Framebuffer framebuffer
     ) {
         if (isDepthTestEnabled) {
-            // TODO!
+            Rasterizer.getInstance().drawDepthTestLine(
+                    x1, y1, z1,
+                    x2, y2, z2,
+                    framebuffer,
+                    new org.openjsr.core.Color(255, 0, 0)
+            );
         } else {
             // За неимением проверенной реализации алгоритма Брезенхема, рисуем через Canvas.
             // TODO: исправить перекрытие гранями (slope.obj, caracal_cube.obj).
