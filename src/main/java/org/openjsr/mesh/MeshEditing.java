b0c40dd485dfc1d2002c1abd91df70b2bb6e6a59
heathlisss <heathlisss@gmail.com>
2024-01-07 17:26:50 +0300

    удаление вершины из модели

--- src/main/java/org/openjsr/mesh/MeshEditing.java
+++ src/main/java/org/openjsr/mesh/MeshEditing.java
@@ -4,8 +4,21 @@ import org.openjsr.mesh.triangulation.SimpleTriangulator;
 import org.openjsr.mesh.triangulation.TriangulatedMesh;
 
 public class MeshEditing {
-    public void removeVertex(TriangulatedMesh model, int idVertex) {
+    public void removeVertex(TriangulatedMesh mesh, int idVertex) {
+        mesh.vertices.remove(idVertex);
+        for (Face face : mesh.faces) {
+            if (face.getVertexIndices().contains(idVertex)) {
+                int vertexIdIdThePolygon = face.getVertexIndices().indexOf(idVertex);
+                if (face.getNormalIndices() != null) face.getNormalIndices().remove(vertexIdIdThePolygon);
+                if (face.getTextureVertexIndices() != null) face.getTextureVertexIndices().remove(vertexIdIdThePolygon);
+                face.getVertexIndices().remove(vertexIdIdThePolygon);
 
+                for (int indexVertex = 0; indexVertex < face.getVertexIndices().size(); indexVertex++) {
+                    if (face.getVertexIndices().get(indexVertex) > idVertex)
+                        face.getVertexIndices().add(indexVertex, face.getVertexIndices().get(indexVertex) - 1);
+                }
+            }
+        }
     }
 
     public void removeFace(TriangulatedMesh mesh, int idFace) {
