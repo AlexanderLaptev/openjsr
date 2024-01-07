8fb80bd8d40bd004cae57805fbb9c0646460fa75
imbeer <76579340+imbeer@users.noreply.github.com>
2024-01-08 01:27:59 +0300

    класс light menu переименован в light storage и теперь является хранилищем созданных пользователем моделей освещения

---
+++ src/main/java/org/openjsr/app/LightStorage.java
@@ -0,0 +1,92 @@
+package org.openjsr.app;
+
+import cg.vsu.render.math.vector.Vector3f;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import org.openjsr.render.lighting.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static javafx.collections.FXCollections.*;
+
+/**
+ * Класс - хранилище созданных пользователем моделей освещения.
+ */
+public class LightStorage {
+
+    /**
+     * Список всех моделей освещения.
+     */
+    public List<LightingModel> models = new ArrayList<>();
+
+    public LightStorage() {}
+
+    /**
+     * Вызывает диалоговое окно с предложением выбрать тип освещения.
+     * @return номер строки в списке, которую выбрал пользователь.
+     */
+    public int addLightningModelDialog() {
+        Dialog<Integer> addDialog = new Dialog<>();
+        addDialog.setTitle("Выберите тип модели освещения");
+
+        ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        addDialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
+
+        VBox dialogContent = new VBox();
+
+        ComboBox<String> typesBox = new ComboBox<>();
+        typesBox.setItems(
+                observableList(
+                        Arrays.asList(
+                                "Равномерное освещение",
+                                "Плоское освещение",
+                                "Гладкое освещение"
+                        )
+                )
+        );
+
+        dialogContent.getChildren().add(typesBox);
+
+        addDialog.getDialogPane().setContent(dialogContent);
+
+        addDialog.setResultConverter(dialogButton -> {
+            if (dialogButton == okButtonType) {
+                return typesBox.getSelectionModel().getSelectedIndex();
+            }
+            return null;
+        });
+
+        return addDialog.showAndWait().get();
+    }
+
+    /**
+     * Выбирает тип модели из доступных.
+     * @param input номер модели в списке.
+     */
+    public void chooseModel(Integer input) {
+
+        switch (input) {
+            case 0 -> {
+                UniformLightingModel model = new UniformLightingModel();
+                models.add(model);
+            }
+            case 1 -> {
+                FlatDirectionalLightingModel model = new FlatDirectionalLightingModel();
+                model.direction = new Vector3f(0, -1, 0);
+                models.add(model);
+
+            }
+            case 2 -> {
+                SmoothDirectionalLightingModel model = new SmoothDirectionalLightingModel();
+                model.direction = new Vector3f(0, -1, 0);
+                models.add(model);
+            }
+            default -> {
+                return;
+            }
+        }
+
+    }
+}
