da0a0b1136515a84f8d2ca291a61fa7ade4ef60e
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-01-09 00:23:55 +0300

    чистка кода MeshEditor

--- src/main/java/org/openjsr/mesh/MeshEditor.java
+++ src/main/java/org/openjsr/mesh/MeshEditor.java
@@ -7,36 +7,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MeshEditor {
-
     public static class MeshEditorException extends RuntimeException {
         public MeshEditorException(String message) {
             super(message);
         }
     }
-    public void removeVertex(TriangulatedMesh mesh, int idVertex) {
-        if (mesh.vertices.size() <= idVertex || idVertex < 0) {
+
+    public void removeVertex(TriangulatedMesh mesh, int vertexIndex) {
+        if (mesh.vertices.size() <= vertexIndex || vertexIndex < 0) {
             throw new MeshEditorException("Такой вершины не существует");
         }
-        mesh.vertices.remove(idVertex);
-        List<Face> removeFace = new ArrayList<>();
+        mesh.vertices.remove(vertexIndex);
+        List<Face> facesToRemove = new ArrayList<>();
+
         for (Face face : mesh.faces) {
-            if (face.getVertexIndices().contains(idVertex) && face.getVertexIndices().size() > 3) {
-                int vertexIdInThePolygon = face.getVertexIndices().indexOf(idVertex);
-                if (!face.getNormalIndices().isEmpty()) face.getNormalIndices().remove(vertexIdInThePolygon);
-                if (!face.getTextureVertexIndices().isEmpty())
-                    face.getTextureVertexIndices().remove(vertexIdInThePolygon);
-                face.getVertexIndices().remove(vertexIdInThePolygon);
+            if (face.getVertexIndices().contains(vertexIndex)
+                    && face.getVertexIndices().size() > 3
+            ) {
+                int faceVertexIndex = face.getVertexIndices().indexOf(vertexIndex);
+                if (!face.getNormalIndices().isEmpty()) {
+                    face.getNormalIndices().remove(faceVertexIndex);
+                }
+                if (!face.getTextureVertexIndices().isEmpty()) {
+                    face.getTextureVertexIndices().remove(faceVertexIndex);
+                }
+                face.getVertexIndices().remove(faceVertexIndex);
             }
-            if (face.getVertexIndices().size() <= 3 && face.getVertexIndices().contains(idVertex))
-                removeFace.add(face);
-            else for (int indexVertex = 0; indexVertex < face.getVertexIndices().size(); indexVertex++) {
-                if (face.getVertexIndices().get(indexVertex) > idVertex) {
-                    face.getVertexIndices().set(indexVertex, face.getVertexIndices().get(indexVertex) - 1);
+
+            if (face.getVertexIndices().size() <= 3
+                    && face.getVertexIndices().contains(vertexIndex)
+            ) {
+                facesToRemove.add(face);
+            } else {
+                for (int faceVertexIndex = 0; faceVertexIndex < face.getVertexIndices().size(); faceVertexIndex++) {
+                    if (face.getVertexIndices().get(faceVertexIndex) > vertexIndex) {
+                        face.getVertexIndices().set(
+                                faceVertexIndex,
+                                face.getVertexIndices().get(faceVertexIndex) - 1
+                        );
+                    }
                 }
             }
         }
-        mesh.faces.removeAll(removeFace);
+
+        mesh.faces.removeAll(facesToRemove);
         mesh.setPolygons(mesh.faces, new SimpleTriangulator());
+        mesh.normals = MeshNormalComputer.getInstance().computeNormals(mesh);
     }
 
     public void removeFace(TriangulatedMesh mesh, int idFace) {
@@ -45,5 +61,6 @@ public class MeshEditor {
         }
         mesh.faces.remove(idFace);
         mesh.setPolygons(mesh.faces, new SimpleTriangulator());
+        mesh.normals = MeshNormalComputer.getInstance().computeNormals(mesh);
     }
 }
